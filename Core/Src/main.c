622a66f3a1096de99333da45dcaa84895f506f3a
guozhanxin <guozhanxin@rt-thread.com>
2022-11-18 02:14:12 +0800

    first version

---
+++ Core/Src/main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022, The Puppy RTOS Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "main.h"
+#include <puppy.h>
+
+int main(void)
+{
+    int board_init(void);
+    board_init();
+
+    while (1)
+    {
+        printk("tick:%d\r\n", HAL_GetTick());
+        HAL_Delay(1000);
+    }
+}
